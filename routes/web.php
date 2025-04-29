4d5312a73045c9e9d8f133940ebb90dd9846598d
Cooljoe464 <Cooljoe464@gmail.com>
2025-04-29 17:28:56 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::get('/maintenance/up', function () {
 
 //Route::middleware(['auth','role:super-admin|admin|librarian|user', 'verified'])->group(function () {
 Route::middleware(['auth', 'role:super-admin|SuperAdmin|admin|librarian|user', 'redirect'])->group(function () {
+
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/manual/sub-manuals/file/{filename}', [HomeController::class, 'downloadSubManuals'])->name('download.submanuals');
     Route::get('/manual/sub-manuals/content/file/{filename}', [HomeController::class, 'downloadSubManualsContent'])->name('download.contents');
