012d45445e83b3b76834bca6dbebbf38e0e849c7
Cooljoe464 <Cooljoe464@gmail.com>
2025-08-28 03:25:09 +0100

    Update index.blade.php

--- resources/views/manuals/items/index.blade.php
+++ resources/views/manuals/items/index.blade.php
@@ -33,9 +33,7 @@
                         <th>{{ $Manual->name }}</th>
                         <th>File Type</th>
                         <th>File Size</th>
-                        {{--                        @can('destroy-manual')--}}
                         <th>Action</th>
-                        {{--                        @endcan--}}
                     </tr>
                     </thead>
                     <tbody class="table-border-bottom-0">
@@ -53,20 +51,16 @@
                                         </td>
                                     @else
                                         <td>
-                                            {{--                                            <a href="{{ asset(str_replace('public/', 'storage/',$items->link)) }}">{{ $items->name }}</a>--}}
                                             <div class="btn-group">
-                                                <a href="{{ route('download.submanuals',$items->link) }}">{{ $items->name }}</a>
-{{--                                                @if($items->file_type === 'application/pdf')--}}
-{{--                                                    &nbsp; &nbsp;--}}
-{{--                                                    <button class=" badge btn-primary cache-doc-btn"--}}
-{{--                                                            data-doc-id="{{ $items->miid }}"--}}
-{{--                                                            data-doc-name="{{ $items->name }}"--}}
-{{--                                                            data-doc-path="{{ $items->link }}"--}}
-{{--                                                            data-pwa-url="{{ getPwaSubManualUrl($items->link) }}"--}}
-{{--                                                            title="Cache this document for offline access">--}}
-{{--                                                        <i class="mdi mdi-download"></i>--}}
-{{--                                                    </button>--}}
-{{--                                                @endif--}}
+                                                @if($items->file_type === 'application/pdf')
+                                                    <a class="btn btn-link p-0 text-start pdf-modal-trigger"
+                                                       href="{{ route('manual.items.view', ['filename' => $items->link]) }}">
+                                                        <i class="mdi mdi-file-pdf-box text-danger me-1"></i>
+                                                        {{ $items->name }}
+                                                    </a>
+                                                @else
+                                                    <a href="{{ route('download.submanuals',$items->link) }}">{{ $items->name }}</a>
+                                                @endif
                                             </div>
                                         </td>
                                     @endif
@@ -111,165 +105,196 @@
         </div>
         <!--/ Responsive Datatable -->
     </div>
+
+    <!-- PDF Viewer Modal -->
+    <div class="modal fade" id="pdfViewerModal" tabindex="-1" aria-labelledby="pdfViewerModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-fullscreen-lg-down">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="pdfViewerModalLabel">PDF Viewer</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body p-0">
+                    <iframe id="pdf-iframe" src="" frameborder="0" width="100%" style="height: 80vh;"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
     @push('scripts')
         <script>
-            $("#dt-responsive").DataTable({
-                "responsive": true, "lengthChange": false, "autoWidth": false, ordering: false,
-                buttons: [
-                    'pageLength'
-                    @can('create-manual')
-                    ,
-                    {
-                        html: '<a class="btn btn-primary" href="{{ route('manual.items.show', $Id) }}"><span class="fa fa-plus-circle" aria-hidden="true"></span> ' +
-                            ' <i class="menu-icon tf-icons mdi mdi-book-account"></i>' +
-                            ' <i class="menu-icon tf-icons mdi mdi-file-content"></i>' +
-                            ' </a>',
-                        attr: {
-                            class: 'btn btn-primary'
-                        },
+            $(document).ready(function() {
+                $("#dt-responsive").DataTable({
+                    "responsive": true, "lengthChange": false, "autoWidth": false, ordering: false,
+                    buttons: [
+                        'pageLength'
+                        @can('create-manual')
+                        ,
+                        {
+                            html: '<a class="btn btn-primary" href="{{ route('manual.items.show', $Id) }}"><span class="fa fa-plus-circle" aria-hidden="true"></span> ' +
+                                ' <i class="menu-icon tf-icons mdi mdi-book-account"></i>' +
+                                ' <i class="menu-icon tf-icons mdi mdi-file-content"></i>' +
+                                ' </a>',
+                            attr: {
+                                class: 'btn btn-primary'
+                            },
+                        }
+                        @endcan
+                    ],
+                    "language": {
+                        "emptyTable": "<span class='text-success'>Empty table</span>"
                     }
-                    @endcan
-                ],
-                "language": {
-                    "emptyTable": "<span class='text-success'>Empty table</span>"
-                }
-            }).buttons().container().appendTo('#dt-responsive_wrapper .col-md-6:eq(0)');
+                }).buttons().container().appendTo('#dt-responsive_wrapper .col-md-6:eq(0)');
 
-            // Individual document caching functionality
-            $(document).on('click', '.cache-doc-btn', function (e) {
-                e.preventDefault();
-
-                const $btn = $(this);
-                const docId = $btn.data('doc-id');
-                const docName = $btn.data('doc-name');
-                const docPath = $btn.data('doc-path');
-                const pwaUrl = $btn.data('pwa-url');
-
-                console.log('Caching document:', {docId, docName, docPath, pwaUrl});
-
-                // Show loading state
-                const originalHtml = $btn.html();
-                $btn.html('<i class="mdi mdi-loading mdi-spin"></i> Caching...');
-                $btn.prop('disabled', true);
-
-                // Cache individual document
-                cacheIndividualDocument({
-                    id: docId,
-                    name: docName,
-                    file_path: docPath,
-                    pwa_url: pwaUrl,
-                    file_type: 'application/pdf'
-                }).then(() => {
-                    showNotification('success', `Document "${docName}" cached successfully!`);
-
-                    // Update button to show cached state
-                    $btn.html('<i class="mdi mdi-check"></i> Cached');
-                    $btn.removeClass('btn-outline-primary').addClass('btn-success');
-                    $btn.prop('disabled', false);
-
-                    // Reset button after 3 seconds
-                    setTimeout(() => {
-                        $btn.html(originalHtml);
-                        $btn.removeClass('btn-success').addClass('btn-outline-primary');
-                    }, 3000);
-
-                }).catch(error => {
-                    console.error('Failed to cache document:', error);
-                    showNotification('error', `Failed to cache "${docName}". Please try again.`);
-
-                    // Reset button
-                    $btn.html(originalHtml);
-                    $btn.prop('disabled', false);
+                // Handle PDF link clicks
+                $('.pdf-modal-trigger').on('click', function(e) {
+                    e.preventDefault();
+                    const pdfUrl = $(this).attr('href');
+                    $('#pdf-iframe').attr('src', pdfUrl);
+                    $('#pdfViewerModal').modal('show');
                 });
-            });
 
-            // Cache individual document function
-            async function cacheIndividualDocument(docData) {
-                try {
-                    // Store in IndexedDB first
-                    if (window.libraryStorage) {
-                        await window.libraryStorage.storeManualItem(docData);
-                    }
+                // Clear iframe src when modal is hidden to stop loading
+                $('#pdfViewerModal').on('hidden.bs.modal', function () {
+                    $('#pdf-iframe').attr('src', '');
+                });
 
-                    // Update auth token first
-                    await updateServiceWorkerAuthToken();
+                // Individual document caching functionality
+                $(document).on('click', '.cache-doc-btn', function (e) {
+                    e.preventDefault();
 
-                    // Cache via service worker
-                    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-                        return new Promise((resolve, reject) => {
-                            const messageChannel = new MessageChannel();
+                    const $btn = $(this);
+                    const docId = $btn.data('doc-id');
+                    const docName = $btn.data('doc-name');
+                    const docPath = $btn.data('doc-path');
+                    const pwaUrl = $btn.data('pwa-url');
 
-                            messageChannel.port1.onmessage = function (event) {
-                                if (event.data.success) {
-                                    resolve(event.data);
-                                } else {
-                                    reject(new Error(event.data.error || 'Caching failed'));
-                                }
-                            };
+                    console.log('Caching document:', {docId, docName, docPath, pwaUrl});
 
-                            navigator.serviceWorker.controller.postMessage({
-                                type: 'CACHE_INDIVIDUAL_DOCUMENT',
-                                document: docData
-                            }, [messageChannel.port2]);
-                        });
-                    } else {
-                        throw new Error('Service worker not available');
-                    }
-                } catch (error) {
-                    console.error('Document caching error:', error);
-                    throw error;
-                }
-            }
+                    // Show loading state
+                    const originalHtml = $btn.html();
+                    $btn.html('<i class="mdi mdi-loading mdi-spin"></i> Caching...');
+                    $btn.prop('disabled', true);
 
-            // Update service worker authentication token
-            async function updateServiceWorkerAuthToken() {
-                if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+                    // Cache individual document
+                    cacheIndividualDocument({
+                        id: docId,
+                        name: docName,
+                        file_path: docPath,
+                        pwa_url: pwaUrl,
+                        file_type: 'application/pdf'
+                    }).then(() => {
+                        showNotification('success', `Document "${docName}" cached successfully!`);
+
+                        // Update button to show cached state
+                        $btn.html('<i class="mdi mdi-check"></i> Cached');
+                        $btn.removeClass('btn-outline-primary').addClass('btn-success');
+                        $btn.prop('disabled', false);
+
+                        // Reset button after 3 seconds
+                        setTimeout(() => {
+                            $btn.html(originalHtml);
+                            $btn.removeClass('btn-success').addClass('btn-outline-primary');
+                        }, 3000);
+
+                    }).catch(error => {
+                        console.error('Failed to cache document:', error);
+                        showNotification('error', `Failed to cache "${docName}". Please try again.`);
+
+                        // Reset button
+                        $btn.html(originalHtml);
+                        $btn.prop('disabled', false);
+                    });
+                });
+
+                // Cache individual document function
+                async function cacheIndividualDocument(docData) {
                     try {
-                        const response = await fetch('/pwa/auth-token', {
-                            credentials: 'same-origin',
-                            headers: {
-                                'X-Requested-With': 'XMLHttpRequest'
-                            }
-                        });
+                        // Store in IndexedDB first
+                        if (window.libraryStorage) {
+                            await window.libraryStorage.storeManualItem(docData);
+                        }
 
-                        if (response.ok) {
-                            const tokenData = await response.json();
+                        // Update auth token first
+                        await updateServiceWorkerAuthToken();
 
-                            navigator.serviceWorker.controller.postMessage({
-                                type: 'SET_AUTH_TOKEN',
-                                token: tokenData.token,
-                                expires_at: tokenData.expires_at
+                        // Cache via service worker
+                        if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+                            return new Promise((resolve, reject) => {
+                                const messageChannel = new MessageChannel();
+
+                                messageChannel.port1.onmessage = function (event) {
+                                    if (event.data.success) {
+                                        resolve(event.data);
+                                    } else {
+                                        reject(new Error(event.data.error || 'Caching failed'));
+                                    }
+                                };
+
+                                navigator.serviceWorker.controller.postMessage({
+                                    type: 'CACHE_INDIVIDUAL_DOCUMENT',
+                                    document: docData
+                                }, [messageChannel.port2]);
                             });
-
-                            return true;
+                        } else {
+                            throw new Error('Service worker not available');
                         }
                     } catch (error) {
-                        console.error('Failed to update auth token:', error);
+                        console.error('Document caching error:', error);
+                        throw error;
                     }
                 }
-                return false;
-            }
 
-            // Show notification function
-            function showNotification(type, message) {
-                const alertClass = type === 'success' ? 'alert-success' : 'alert-danger';
-                const icon = type === 'success' ? 'mdi-check-circle' : 'mdi-alert-circle';
+                // Update service worker authentication token
+                async function updateServiceWorkerAuthToken() {
+                    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+                        try {
+                            const response = await fetch('/pwa/auth-token', {
+                                credentials: 'same-origin',
+                                headers: {
+                                    'X-Requested-With': 'XMLHttpRequest'
+                                }
+                            });
 
-                const notification = $(`
-                    <div class="alert ${alertClass} alert-dismissible position-fixed" style="top: 20px; right: 20px; z-index: 1060; max-width: 350px;">
-                        <i class="mdi ${icon} me-2"></i>
-                        ${message}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                    </div>
-                `);
+                            if (response.ok) {
+                                const tokenData = await response.json();
 
-                $('body').append(notification);
+                                navigator.serviceWorker.controller.postMessage({
+                                    type: 'SET_AUTH_TOKEN',
+                                    token: tokenData.token,
+                                    expires_at: tokenData.expires_at
+                                });
 
-                // Auto-hide after 5 seconds
-                setTimeout(() => {
-                    notification.alert('close');
-                }, 5000);
-            }
+                                return true;
+                            }
+                        } catch (error) {
+                            console.error('Failed to update auth token:', error);
+                        }
+                    }
+                    return false;
+                }
+
+                // Show notification function
+                function showNotification(type, message) {
+                    const alertClass = type === 'success' ? 'alert-success' : 'alert-danger';
+                    const icon = type === 'success' ? 'mdi-check-circle' : 'mdi-alert-circle';
+
+                    const notification = $(`
+                        <div class="alert ${alertClass} alert-dismissible position-fixed" style="top: 20px; right: 20px; z-index: 1060; max-width: 350px;">
+                            <i class="mdi ${icon} me-2"></i>
+                            ${message}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                        </div>
+                    `);
+
+                    $('body').append(notification);
+
+                    // Auto-hide after 5 seconds
+                    setTimeout(() => {
+                        notification.alert('close');
+                    }, 5000);
+                }
+            });
         </script>
     @endpush
 @endsection
